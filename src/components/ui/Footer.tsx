86cee5c8bf395c54e367a527c5d0ce64806790df
Himanshu Verma <hvinprimary@gmail.com>
2025-03-11 16:33:11 +0530

    add favicon in layout.tsx and modify footer.tsx

--- src/components/ui/Footer.tsx
+++ src/components/ui/Footer.tsx
@@ -1,10 +1,16 @@
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-transparent w-full mx-auto p-4 rounded-xl dark:bg-transparent shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] group">
-      <p className="text-xs text-center">
-        © {new Date().getFullYear()} hv
+    <footer className="bg-transparent w-full mx-auto p-4 dark:bg-transparent shadow-[2px_4px_16px_0px_rgba(248,248,248,0.06)_inset] group">
+    <p className="text-xs text-center">
+      © {new Date().getFullYear()} hv
+    </p>
+    <a href="mailto:hvinprimary@gmail.com">
+      <p className="text-xs text-center text-blue-300 hover:underline">
+        collaborate with me
       </p>
-    </footer>
+    </a>
+  </footer>
+  
   );
 };
 
