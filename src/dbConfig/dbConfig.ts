ea80ead031f7a21ff6bcb1729e83e369ab55114f
Himanshu Verma <hvinprimary@gmail.com>
2025-03-13 04:32:41 +0530

    modify dbConfig.ts

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,20 +2,23 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        console.log(process.env.MONGO_URI!)
-        await mongoose.connect(process.env.MONGO_URI!);
+        mongoose.connect(process.env.MONGO_URI!);
         const connection = mongoose.connection;
 
         connection.on('connected', () => {
-            console.log('✅ MongoDB connected successfully');
-        });
+            console.log('MongoDB connected successfully');
+        })
 
         connection.on('error', (err) => {
-            console.error('❌ MongoDB connection error:', err);
-            process.exit(1);
-        });
+            console.log('MongoDB connection error. Please make sure MongoDB is running. ' + err);
+            process.exit();
+        })
 
     } catch (error) {
-        console.error('❌ Something went wrong while connecting to MongoDB:', error);
+        console.log('Something goes wrong!');
+        console.log(error);
+        
     }
+
+
 }
