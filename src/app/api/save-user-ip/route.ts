51eb1dec1986cc3b6f3b2d5cb2854f523c499b4b
Himanshu Verma <hvinprimary@gmail.com>
2025-03-13 03:35:46 +0530

    add api route to save unique users count to db

---
+++ src/app/api/save-user-ip/route.ts
@@ -0,0 +1,24 @@
+import { NextRequest, NextResponse } from "next/server";
+import UniqueViews from "@/model/UniqueViews";
+import { connect } from "@/dbConfig/dbConfig";
+
+// Ensure database connection
+connect();
+
+export async function POST(req: NextRequest) {
+    try {
+        const { ip } = await req.json();
+
+        // Check if the IP is already stored
+        const existingUser = await UniqueViews.findOne({ ip });
+
+        if (!existingUser) {
+            const newUser = new UniqueViews({ ip });
+            await newUser.save();
+        }
+
+        return NextResponse.json({ message: "IP saved successfully" }, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+    }
+}
