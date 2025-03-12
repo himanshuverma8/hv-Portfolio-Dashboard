fa1e8df8185d59299d2d99a6d12725d8938477c1
Himanshu Verma <hvinprimary@gmail.com>
2025-03-13 04:06:21 +0530

    add mongo db config

---
+++ src/app/dbConfig/dbConfig.ts
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+
+export async function connect() {
+    try {
+        if (mongoose.connection.readyState >= 1) {
+            console.log('✅ Already connected to MongoDB.');
+            return;
+        }
+        console.log(process.env.MONGO_URI!)
+        await mongoose.connect(process.env.MONGO_URI!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('✅ MongoDB connected successfully');
+        });
+
+        connection.on('error', (err) => {
+            console.error('❌ MongoDB connection error:', err);
+            process.exit(1);
+        });
+
+    } catch (error) {
+        console.error('❌ Something went wrong while connecting to MongoDB:', error);
+    }
+}
