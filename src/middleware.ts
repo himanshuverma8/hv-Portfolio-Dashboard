a602b0cff9be0d36e4d7daa991ff3235e8da4e4d
Himanshu Verma <hvinprimary@gmail.com>
2025-06-05 18:06:20 +0530

    modify the middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,10 @@ import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const path = request.nextUrl.pathname;
-
+  // allow redirect to the root page route when path starts with /links
+  if(path.startsWith("/links")){
+    return NextResponse.redirect(new URL("/", request.nextUrl));
+  }
   // Allow the "/links" page and static files (e.g., CSS, JS, API, and public assets)
   if (path.startsWith("/") || path.startsWith("/_next") || path.startsWith("/api") || path.startsWith("/public")) {
     return NextResponse.next();
