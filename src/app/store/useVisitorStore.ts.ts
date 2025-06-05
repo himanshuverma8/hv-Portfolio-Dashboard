4c92732cc190f33ef06531d79c5d1b85ee78a801
Himanshu Verma <hvinprimary@gmail.com>
2025-06-05 17:00:05 +0530

    add new global state variable to track the isPlaying song of spotify

--- src/app/store/useVisitorStore.ts.ts
+++ src/app/store/useVisitorStore.ts.ts
@@ -3,8 +3,17 @@ interface VisitorState {
    activeUsers: number;
    setActiveUsers: (count: number) => void;
  }
+interface isPlayingState {
+  isPlaying: boolean;
+  setIsPlaying: (value: boolean) => void;
+}
 
  export const useVisitorStore = create<VisitorState>((set) => ({
    activeUsers: 0,
    setActiveUsers: (count) => set({ activeUsers: count }),
+ }));
+
+ export const useIsPlaying = create<isPlayingState>((set)=>({
+  isPlaying: false,
+  setIsPlaying: (value) => set({isPlaying: value}),
  }));
